330baaa5cfe840f536380cff3b713208206415cb
Joshua Bretag <joshua.bretag@dhxmedia.com>
2018-11-21 16:30:11 -0800

    finished module

--- jobs/app.py
+++ jobs/app.py
@@ -32,7 +32,8 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, '
+                       'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html',jobs=jobs)
 
 
@@ -41,8 +42,35 @@ def jobs():
 def job(job_id):
     job = execute_sql(
 
-            'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?',
+            'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as '
+            'employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?',
             [job_id],
             single=True
     )
     return render_template('job.html', job=job)
+
+app.route('/employer/<employer_id>')
+def employer(employer_id):
+    employer = execute_sql(
+            'SELECT * FROM employer WHERE id=?',
+            [employer_id],
+            single=True
+    )
+    jobs = execute_sql(
+            'SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = '
+            'job.employer_id WHERE employer.id = ?',
+            [employer_id],
+            single=True
+    )
+
+    reviews = execute_sql(
+            'SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id '
+            'WHERE employer.id = ?',
+            [employer_id]
+    )
+
+
+    return render_template('employer.html',employer=employer,jobs=jobs, reviews=reviews)
+
+
+
